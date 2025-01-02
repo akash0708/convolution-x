62602f7f1b61aad82a6d13697604398281c44ebb
Akash09 <akashbag12a05@gmail.com>
2025-01-02 17:21:37 +0530

    events and register

--- app/event/register/page.tsx
+++ app/event/register/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-screen flex items-center justify-center ">
       <EventRegistrationForm />
     </div>
   );
