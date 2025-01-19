dc8af0c3130522242b538d6cf8e7514795f35682
Akash09 <akashbag12a05@gmail.com>
2025-01-19 13:04:23 +0530

    forgot password

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,8 @@
 import { signIn } from "@/lib/auth";
 import { useUserStore } from "@/store/userStore";
 import axios from "axios";
-import { applyActionCode, getAuth } from "firebase/auth";
 import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Suspense, useState } from "react";
 import toast from "react-hot-toast";
 import { IoMdArrowRoundBack } from "react-icons/io";
@@ -82,46 +81,6 @@ export default function RegisterForm() {
           <h2 className="sm:text-2xl text-lg font-bold mb-2">
             Welcome back!
           </h2>
-          <>
-          {/*
-           <p className="text-white/90 text-sm sm:text-base">Choose your registration method</p> */}
-          {/* Google Sign In Button */}
-          {/* <button
-            type="button"
-            onClick={handleGoogleSignIn}
-            className="w-full flex items-center justify-center gap-x-2 bg-transparent border border-gray-300 rounded-full px-4 py-2 transition-colors duration-300  hover:bg-white hover:text-darkBlue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 "
-          >
-            <svg className="size-4 sm:size-5 " fill="currentColor" viewBox="0 0 24 24">
-              <path
-                fill=""
-                d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-              />
-              <path
-                fill=""
-                d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-              />
-              <path
-                fill=""
-                d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-              />
-              <path
-                fill=""
-                d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-              />
-            </svg>
-            <p className="text-sm sm:text-base font-medium">
-              
-              Continue with Google
-            </p>
-          </button> */}
-
-          {/* <div className="flex w-full justify-center items-center">
-            <div className="w-full h-[1px] rounded-l-full bg-white/80"></div>
-            <span className="text-nowrap px-2 text-sm sm:text-base">Or continue with email</span>
-            <div className="w-full h-[1px] rounded-r-full bg-white/80"></div>
-          </div> 
-          */}
-          </>
 
           {/* ------------------email form starts here------------------- */}
           <form
@@ -235,6 +194,13 @@ export default function RegisterForm() {
                 "Log in"
               )}
             </button>
+
+            <Link
+              href="/forgot-password"
+              className="text-center hover:text-white/80 text-sm sm:text-base text-white/90"
+            >
+              Forgot Password?
+            </Link>
           </form>
         </div>
       </div>
