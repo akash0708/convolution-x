5b3cfa3f14b2f14875335b844468d0e837a9c008
Akash09 <akashbag12a05@gmail.com>
2025-01-30 20:42:12 +0530

    seo update

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: "/admin",
+        disallow: ["/admin", "/superadmin"],
       },
     ],
     sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
