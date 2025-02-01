c322551f710961deda4a6518f948fccf652a39f2
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2025-02-01 19:34:06 +0530

    Minor bugs fixed

--- app/event/circuistics/page.tsx
+++ app/event/circuistics/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import ComingSoon from '@/components/ComingSoon'
 import { Protest_Riot } from "next/font/google";
 import Loading from '@/app/loading';
@@ -13,12 +13,27 @@ import PostTimeline from './PostTimeline';
 import FaqCircuistics from './FaqCircuistics';
 import PreFooter from './PreFooter';
 import Footer from './Footer';
+import { useUserStore } from '@/store/userStore';
 const protestRiot = Protest_Riot({
   subsets: ["latin"], // You can specify subsets if needed
   weight: "400", // Adjust the weight based on your requirements
 });
 
 const Page = () => {
+  const { user, authCheck, loading, setIsLogged } = useUserStore();
+    useEffect(() => {
+      // if (!user && email && !isLogged) {
+      //   setLoading(true)
+      //   fetchUser(email);
+      // }
+      if (!user) {
+        setIsLogged(true);
+        authCheck();
+      }
+    }, [user]);
+    if (loading) {
+      return <Loading />;
+    }
   return (
      <Suspense fallback={ <Loading/>}>
       <EventNavbar navTheme="bg-gradient-to-t from-[#3122B1] to-[#150E4B]"/>
